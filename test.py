14e460b6a99ed6efb0f41a100085ecb7c2ce28c5
F.M. Eckardt Ferreira <f.m.eckardt.ferreira@student.rug.nl>
2024-04-24 14:36:59 +0200

    Neware and EClab to DF

---
+++ test.py
@@ -0,0 +1,3 @@
+import NewareNDA as nda
+
+data = nda.read(r'G:\.shortcut-targets-by-id\1gpf-XKVVvMHbMGqpyQS5Amwp9fh8r96B\RUG shared\Master Project\Thesis\Data Figures\Fig_MM_performance\FF042batt_a.ndax')
